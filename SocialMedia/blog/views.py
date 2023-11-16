a3dd5013ec026863197d42ab459fe052963fd540
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2023-11-16 15:28:42 +0200

    day_3

--- SocialMedia/blog/views.py
+++ SocialMedia/blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-posts=[
-    {
-        "author":"youssef",
-        "title":"بسم الله الرحمن الرحيم ",
-        "content":"الله اكبر ",
-        "date_posted":"August 27 ,2018",
-    },
-    {
-        "author":"nader",
-        "title":" welcome",
-        "content":"second post content ",
-        "date_posted":"August 27 ,2018",
-    }
-]
+
 def home(request):
-    context={"posts":posts}
+    context={"posts":Post.objects.all()}
     return render(request,"blog/home.html",context)
 def about(request):
     return render(request,"blog/about.html",{"title":"about"})
