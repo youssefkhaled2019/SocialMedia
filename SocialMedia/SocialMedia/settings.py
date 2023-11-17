60d81e3371fc77f59ea45eaf97cea8f8927b9a78
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2023-11-17 18:11:26 +0200

    day_4

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- SocialMedia/blog/views.py
+++ SocialMedia/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+
 # Create your views here.
 
 def home(request):
