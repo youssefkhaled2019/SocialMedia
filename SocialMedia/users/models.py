74db29925fc360d4b6eed2046af01bc2f458d2da
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2023-11-23 22:14:29 +0200

    day_6

--- SocialMedia/users/models.py
+++ SocialMedia/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+# Create your models here.
+class Profile(models.Model):
+ user_id=models.OneToOneField(User,on_delete=models.CASCADE)
+ image=models.ImageField(default="default.jpg",upload_to="profile_pics")
+ def __str__(self):
+  return f'{ self.user_id.username} profile '
+  
