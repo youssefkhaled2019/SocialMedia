ceef9bd874bcdc934efc7986f81fd784609307ab
test2a2a <149164071+test2a2a@users.noreply.github.com>
2023-11-24 15:41:00 +0200

    day_7

--- SocialMedia/users/models.py
+++ SocialMedia/users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+from PIL import Image
 # Create your models here.
 from django.contrib.auth.models import User
 # Create your models here.
@@ -8,4 +10,11 @@ class Profile(models.Model):
  image=models.ImageField(default="default.jpg",upload_to="profile_pics")
  def __str__(self):
   return f'{ self.user_id.username} profile '
-  
+ 
+ def save(self):
+  super().save()
+  img=Image.open(self.image.path)
+  if img.height>300 or img.width>300:
+   size=(300,300)
+   img.thumbnail(size)
+   img.save(self.image.path)
